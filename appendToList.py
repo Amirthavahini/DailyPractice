23a07976476cc233c9c42576eb45cc0cc6047735
Amirthavahini <amirtha.vahini14@gmail.com>
2025-06-12 13:49:46 +0530

    These are the 11 problems I solved on 11/6/25

---
+++ appendToList.py
@@ -0,0 +1,10 @@
+#Append values to List
+'''
+You are given three inputs a, b, c. You need to create a list and append a, b, c to the list and then return that list.
+'''
+def appendToList(a,b,c):
+    arr = []
+    arr.append(a)
+    arr.append(b)
+    arr.append(c)
+    return arr
